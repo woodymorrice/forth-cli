e7e6f7f413d63359adad71b4e0fd9628e45eb9e4
Woody Morrice <wam553@usask.ca>
2022-06-14 22:10:44 -0600

    Makefile debug target and all dependencies tested and working properly

--- bin/test.sh
+++ bin/test.sh
@@ -35,3 +35,32 @@ make squeaky clean
 diff test/test1.act test/test1.out | tee test/test1.result
 rm -f test/test1.act
 
+########################################################
+# Test 2 - Testing Makefile debug target and dependencies
+# Test each target, if any errors are reported, test failed
+
+make  out/forth-g.o > test/test2.act
+make squeaky clean
+
+make  out/stack-g.o >> test/test2.act
+make squeaky clean
+
+make  out/dict-g.o >> test/test2.act
+make squeaky clean
+
+make  out/datum-g.o >> test/test2.act
+make squeaky clean
+
+make  bin/forth-debug >> test/test2.act
+make squeaky clean
+
+make  debug >> test/test2.act
+make squeaky clean
+
+make  all >> test/test2.act
+make squeaky clean
+
+
+diff test/test2.act test/test2.out | tee test/test2.result
+rm -f test/test2.act
+
