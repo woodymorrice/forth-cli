30b5f8e2e8d47ffcf7d02d017057a17965a65226
Woody Morrice <wam553@usask.ca>
2022-06-17 21:40:43 -0600

    Q6: Tests 3 & 5 pass * properly handles non-booleans * the true condition without nesting works

--- bin/test.sh
+++ bin/test.sh
@@ -10,66 +10,66 @@
 # Test 1 - Testing Makefile
 # Test each target, if any errors are reported, test failed
 
-make  out/forth.o > test/test1.act
-make squeaky clean
+# make  out/forth.o > test/test1.act
+# make squeaky clean
 
-make  out/stack.o >> test/test1.act
-make squeaky clean
+# make  out/stack.o >> test/test1.act
+# make squeaky clean
 
-make  out/dict.o >> test/test1.act
-make squeaky clean
+# make  out/dict.o >> test/test1.act
+# make squeaky clean
 
-make  out/datum.o >> test/test1.act
-make squeaky clean
+# make  out/datum.o >> test/test1.act
+# make squeaky clean
 
-make  bin/forth >> test/test1.act
-make squeaky clean
+# make  bin/forth >> test/test1.act
+# make squeaky clean
 
-make  forth >> test/test1.act
-make squeaky clean
+# make  forth >> test/test1.act
+# make squeaky clean
 
-make  all >> test/test1.act
-make squeaky clean
+# make  all >> test/test1.act
+# make squeaky clean
 
 
-diff test/test1.act test/test1.out | tee test/test1.result
-rm -f test/test1.act
+# diff test/test1.act test/test1.out | tee test/test1.result
+# rm -f test/test1.act
 
 ########################################################
 # Test 2 - Testing Makefile debug target and dependencies
 # Test each target, if any errors are reported, test failed
 
-make  out/forth-g.o > test/test2.act
-make squeaky clean
+# make  out/forth-g.o > test/test2.act
+# make squeaky clean
 
-make  out/stack-g.o >> test/test2.act
-make squeaky clean
+# make  out/stack-g.o >> test/test2.act
+# make squeaky clean
 
-make  out/dict-g.o >> test/test2.act
-make squeaky clean
+# make  out/dict-g.o >> test/test2.act
+# make squeaky clean
 
-make  out/datum-g.o >> test/test2.act
-make squeaky clean
+# make  out/datum-g.o >> test/test2.act
+# make squeaky clean
 
-make  bin/forth-debug >> test/test2.act
-make squeaky clean
+# make  bin/forth-debug >> test/test2.act
+# make squeaky clean
 
-make  debug >> test/test2.act
-make squeaky clean
+# make  debug >> test/test2.act
+# make squeaky clean
 
-make  all >> test/test2.act
-make squeaky clean
+# make  all >> test/test2.act
+# make squeaky clean
 
 
-diff test/test2.act test/test2.out | tee test/test2.result
-rm -f test/test2.act
+# diff test/test2.act test/test2.out | tee test/test2.result
+# rm -f test/test2.act
 
 
 ########################################################
 # Test 3 - Testing if/else/then
 # popped value is not a bool
 
-bin/forth  <  test.test3.in   \
+bin/forth  <  test/test3.in   \
 | diff -      test/test3.out  \
 | tee         test/test3.result
 
@@ -78,6 +78,22 @@ bin/forth  <  test.test3.in   \
 # Test 4 - Testing if/else/then
 # testing both conditions and nested statements
 
-bin/forth  <  test.test4.in   \
+bin/forth  <  test/test4.in   \
 | diff -      test/test4.out  \
 | tee         test/test4.result
+
+########################################################
+# Test 5 - Testing if/else/then
+# testing true statement to make sure it works
+
+bin/forth  <  test/test5.in   \
+| diff -      test/test5.out  \
+| tee         test/test5.result
+
+########################################################
+# Test 6 - Testing if/else/then
+# testing false statement to make sure it works
+
+bin/forth  <  test/test6.in   \
+| diff -      test/test6.out  \
+| tee         test/test6.result
